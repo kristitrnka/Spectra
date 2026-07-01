af15db64ed327ed56cd075a2266a94d358df9f2b
Kristihack <kristitrnka@gmail.com>
2026-07-01 12:11:04 +0200

    Make Oculus load as Forge mod on 1.12.2

---
+++ build/generated/sources/injectTags/net/coderbot/iris/Tags.java
@@ -0,0 +1,11 @@
+package net.coderbot.iris;
+
+/** Auto-generated tags from RetroFuturaGradle */
+public class Tags {
+    private Tags() {}
+
+    /** Auto-generated tag from RetroFuturaGradle */
+    public static final String MOD_VERSION = "1.4.7";
+    /** Auto-generated tag from RetroFuturaGradle */
+    public static final String MOD_NAME = "Vintagium";
+}
