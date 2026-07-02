2565c0a3fc7f0fa1029bc51c37487df5a8abcb9c
Kristihack <kristitrnka@gmail.com>
2026-07-02 11:55:19 +0200

    Set common uniforms for terrain pipeline hooks

--- src/main/java/net/coderbot/iris/spectra/mixin/MixinRenderGlobalSpectraTerrain.java
+++ src/main/java/net/coderbot/iris/spectra/mixin/MixinRenderGlobalSpectraTerrain.java
@@ -1,6 +1,7 @@
 package net.coderbot.iris.spectra.mixin;
 
 import net.coderbot.iris.Iris;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockRenderLayer;
@@ -18,6 +19,8 @@ public class MixinRenderGlobalSpectraTerrain {
 
     private static boolean spectraLoggedTerrain = false;
     private static boolean spectraLoggedWater = false;
+    private static int spectraFrameCounter = 0;
+    private static final long SPECTRA_START_TIME_NANOS = System.nanoTime();
 
     @Inject(method = "func_174977_a", at = @At("HEAD"), remap = false)
     private void spectra$bindTerrainShader(BlockRenderLayer blockLayerIn, double partialTicks, int pass, Entity entityIn, CallbackInfoReturnable<Integer> cir) {
@@ -31,6 +34,7 @@ public class MixinRenderGlobalSpectraTerrain {
         spectraBoundTerrainProgram = true;
 
         GL20.glUseProgram(programId);
+        spectra$setupCommonUniforms(programId);
 
         if (blockLayerIn == BlockRenderLayer.TRANSLUCENT) {
             if (!spectraLoggedWater) {
@@ -54,6 +58,46 @@ public class MixinRenderGlobalSpectraTerrain {
         GL20.glUseProgram(spectraPreviousTerrainProgram);
         spectraPreviousTerrainProgram = 0;
         spectraBoundTerrainProgram = false;
+    }
+
+    private static void spectra$setupCommonUniforms(int programId) {
+        Minecraft minecraft = Minecraft.getMinecraft();
+
+        int width = Math.max(1, minecraft.displayWidth);
+        int height = Math.max(1, minecraft.displayHeight);
+
+        float frameTimeCounter = (System.nanoTime() - SPECTRA_START_TIME_NANOS) / 1000000000.0F;
+        int worldTime = minecraft.world != null ? (int) (minecraft.world.getWorldTime() % 24000L) : 0;
+
+        spectraFrameCounter++;
+
+        spectra$uniform1i(programId, "texture", 0);
+        spectra$uniform1i(programId, "gtexture", 0);
+        spectra$uniform1i(programId, "colortex0", 0);
+
+        spectra$uniform1f(programId, "viewWidth", width);
+        spectra$uniform1f(programId, "viewHeight", height);
+        spectra$uniform1f(programId, "screenWidth", width);
+        spectra$uniform1f(programId, "screenHeight", height);
+        spectra$uniform1f(programId, "aspectRatio", (float) width / (float) height);
+        spectra$uniform1f(programId, "frameTimeCounter", frameTimeCounter);
+
+        spectra$uniform1i(programId, "worldTime", worldTime);
+        spectra$uniform1i(programId, "frameCounter", spectraFrameCounter);
+    }
+
+    private static void spectra$uniform1i(int programId, String name, int value) {
+        int location = GL20.glGetUniformLocation(programId, name);
+        if (location >= 0) {
+            GL20.glUniform1i(location, value);
+        }
+    }
+
+    private static void spectra$uniform1f(int programId, String name, float value) {
+        int location = GL20.glGetUniformLocation(programId, name);
+        if (location >= 0) {
+            GL20.glUniform1f(location, value);
+        }
     }
 
     private static int spectra$getProgramForLayer(BlockRenderLayer layer) {
